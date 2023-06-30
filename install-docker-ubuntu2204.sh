012602380974c66e0de4ac68c1b5eb32d0f846a2
pelandrun <51940210+pelandrun@users.noreply.github.com>
2023-06-30 14:59:12 -0300

    Update install-docker-ubuntu2204.sh

--- install-docker-ubuntu2204.sh
+++ install-docker-ubuntu2204.sh
@@ -15,3 +15,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+ echo '{
+  "userns-remap": "default"
+}' | sudo tee /etc/docker/daemon.json
+
